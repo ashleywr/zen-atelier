08d8f4e2eda0fa6c01fab95968f5ad9f8e2935c2
Ashley Wijnen-Riems <ashleywr20@gmail.com>
2026-06-15 18:34:37 +0900

    Delete space, zone, and room packages

--- src/main/java/com/sanhiruzu/atelier/space/analyze/AnalysisResult.java
+++
@@ -1,5 +0,0 @@
-package com.sanhiruzu.atelier.space.analyze;
-
-import java.util.List;
-
-public record AnalysisResult(int chunkX, int chunkZ, long snapshotVersion, List<MicroRegion> microRegions) {}
